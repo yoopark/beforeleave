dd81cd67aaf565b05ebdc27bac039a960ae1610f
Yongjun Park <yopark@student.42seoul.kr>
2024-01-09 22:11:05 +0900

    feat: shortcut for toggling the comment dialog

--- background.js
+++ background.js
@@ -9,3 +9,19 @@ chrome.action.onClicked.addListener((tab) => {
     files: ['onclick-badge.js'],
   });
 });
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'toggle-dialog') {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tab = tabs[0];
+
+      if (tab.url.startsWith('chrome://')) {
+        return;
+      }
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['onclick-badge.js'],
+      });
+    });
+  }
+});
